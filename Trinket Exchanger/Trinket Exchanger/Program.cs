0bb75a69a36fb89ebc55ef00547be0c95d171cb0
Snowy Dr3amer <tofu@dr3amer.de>
2015-02-12 09:00:32 +0100

    fixes

--- Trinket Exchanger/Trinket Exchanger/Program.cs
+++ Trinket Exchanger/Trinket Exchanger/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using LeagueSharp;
 using LeagueSharp.Common;
 using SharpDX;
-
 using Color = System.Drawing.Color;
 
 namespace Trinket_Exchanger
