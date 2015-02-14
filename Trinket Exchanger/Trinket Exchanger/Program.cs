0f096ca14e9d8b55db245be71877fb0a151cb44f
Snowy Dr3amer <tofu@dr3amer.de>
2015-02-14 11:19:33 +0100

    dunno

--- Trinket Exchanger/Trinket Exchanger/Program.cs
+++ Trinket Exchanger/Trinket Exchanger/Program.cs
@@ -11,7 +11,7 @@ namespace Trinket_Exchanger
     class Program
     {
         private static Obj_AI_Hero Player { get { return ObjectManager.Player; } }
-        public static Menu menu;
+        public static Menu _menu;
 
         static void Main(string[] args)
         {
@@ -20,15 +20,15 @@ namespace Trinket_Exchanger
 
         static void Game_OnGameLoad(EventArgs args)
         {
-            menu = new Menu("Trinket Exchanger", "TrinketExchanger", true);
-            menu.AddItem(new MenuItem("yellow", "Buy Yellow at start").SetValue(true));
-            menu.AddItem(new MenuItem("red", "Buy Red").SetValue(true));
-            menu.AddItem(new MenuItem("redtimer", "Buy Red at min:").SetValue(new Slider(15, 1, 60)));
-            menu.AddItem(new MenuItem("blue", "Buy Blue").SetValue(true));
-            menu.AddItem(new MenuItem("bluetimer", "Buy Blue at min:").SetValue(new Slider(30, 1, 60)));
-            menu.AddItem(new MenuItem("redSightstone", "Buy Red on Sightstone").SetValue(true));
-            menu.AddItem(new MenuItem("adc", "Buy Blue when IE + Zeal is finished").SetValue(true));
-            menu.AddToMainMenu();
+            _menu = new Menu("Trinket Exchanger", "TrinketExchanger", true);
+            _menu.AddItem(new MenuItem("yellow", "Buy Yellow at start").SetValue(true));
+            _menu.AddItem(new MenuItem("red", "Buy Red").SetValue(true));
+            _menu.AddItem(new MenuItem("redtimer", "Buy Red at min:").SetValue(new Slider(15, 1, 60)));
+            _menu.AddItem(new MenuItem("blue", "Buy Blue").SetValue(true));
+            _menu.AddItem(new MenuItem("bluetimer", "Buy Blue at min:").SetValue(new Slider(30, 1, 60)));
+            _menu.AddItem(new MenuItem("redSightstone", "Buy Red on Sightstone").SetValue(true));
+            _menu.AddItem(new MenuItem("adc", "Buy Blue when IE + Zeal is finished").SetValue(true));
+            _menu.AddToMainMenu();
             Game.PrintChat("Trinket Exchanger loaded.");
             Game.OnGameUpdate += OnTick;
         }
@@ -37,29 +37,30 @@ namespace Trinket_Exchanger
         {
             if (Player.IsDead || Player.InShop())
             {
-                if (GetTimer() < 2 && menu.Item("yellow").GetValue<bool>() && !Player.InventoryItems.Find(s => s.Id == ItemId.Warding_Totem_Trinket).IsValidSlot())
+                if (GetTimer() <= 1 && _menu.Item("yellow").GetValue<bool>() && !Player.InventoryItems.Find(s => s.Id == ItemId.Warding_Totem_Trinket).IsValidSlot() 
+                    && !Player.InventoryItems.Find(s => s.Id == ItemId.Scrying_Orb_Trinket).IsValidSlot() && !Player.InventoryItems.Find(s => s.Id == ItemId.Sweeping_Lens_Trinket).IsValidSlot())
                 {
                     Player.BuyItem(ItemId.Warding_Totem_Trinket);
                 }
-                if (menu.Item("red").GetValue<bool>() && (GetTimer() >= menu.Item("redtimer").GetValue<Slider>().Value) &&
-                    (GetTimer() < menu.Item("bluetimer").GetValue<Slider>().Value) && !Player.InventoryItems.Find(s => s.Id == ItemId.Sweeping_Lens_Trinket).IsValidSlot())
+                if (_menu.Item("red").GetValue<bool>() && (GetTimer() >= _menu.Item("redtimer").GetValue<Slider>().Value) &&
+                    (GetTimer() < _menu.Item("bluetimer").GetValue<Slider>().Value) && !Player.InventoryItems.Find(s => s.Id == ItemId.Sweeping_Lens_Trinket).IsValidSlot())
                 {
                     Player.BuyItem(ItemId.Sweeping_Lens_Trinket);
                 }
-                if (menu.Item("blue").GetValue<bool>() && (GetTimer() >= menu.Item("bluetimer").GetValue<Slider>().Value) 
+                if (_menu.Item("blue").GetValue<bool>() && (GetTimer() >= _menu.Item("bluetimer").GetValue<Slider>().Value) 
                     && !Player.InventoryItems.Find(s => s.Id == ItemId.Scrying_Orb_Trinket).IsValidSlot())
                 {
                     Player.BuyItem(ItemId.Scrying_Orb_Trinket);
                 }
-                if (menu.Item("red").GetValue<bool>() && menu.Item("redSightstone").GetValue<bool>() && Player.InventoryItems.Find(s => s.Id == ItemId.Sightstone).IsValidSlot()
+                if (_menu.Item("red").GetValue<bool>() && _menu.Item("redSightstone").GetValue<bool>() && Player.InventoryItems.Find(s => s.Id == ItemId.Sightstone).IsValidSlot()
                     || Player.InventoryItems.Find(s => s.Id == ItemId.Ruby_Sightstone).IsValidSlot() && !Player.InventoryItems.Find(s => s.Id == ItemId.Sweeping_Lens_Trinket).IsValidSlot())
                 {
                     Player.BuyItem(ItemId.Sweeping_Lens_Trinket);
                 }
-                if (menu.Item("blue").GetValue<bool>() && menu.Item("adc").GetValue<bool>() && Player.InventoryItems.Find(s => s.Id == ItemId.Infinity_Edge).IsValidSlot()
+                if (_menu.Item("blue").GetValue<bool>() && _menu.Item("adc").GetValue<bool>() && Player.InventoryItems.Find(s => s.Id == ItemId.Infinity_Edge).IsValidSlot()
                     && Player.InventoryItems.Find(s => s.Id == ItemId.Zeal).IsValidSlot() && !Player.InventoryItems.Find(s => s.Id == ItemId.Scrying_Orb_Trinket).IsValidSlot())
                 {
-                    menu.Item("red").SetValue(false);
+                    _menu.Item("red").SetValue(false);
                     Player.BuyItem(ItemId.Scrying_Orb_Trinket);
                 }
             }
